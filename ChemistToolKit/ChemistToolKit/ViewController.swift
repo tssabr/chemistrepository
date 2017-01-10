a9e0fbf94f5b6fec247e18de302a1ad5f5b61937
IamTinyTurtle <tss.abr@gmail.com>
2017-01-09 19:27:33 -0500

    Created Project
    created iOS project for chemist toolkit

---
+++ ChemistToolKit/ChemistToolKit/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ChemistToolKit
+//
+//  Created by Adam Ribblett on 1/9/17.
+//  Copyright © 2017 IamAdamRibblett. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
